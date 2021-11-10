33dde6a67978e90ca4370f0e085535ee57aaa5d7
Shahrzad Samimi <shahrzadd@gmail.com>
2021-11-10 16:35:39 -0500

    3rd commit - Add 'while' statement

--- calc.sh
+++ calc.sh
@@ -5,3 +5,12 @@ echo "Welcome to my calculator. My name is Shahrzad"
 echo "May I ask your name please?"
 read name
 echo "It's great connecting with you $name"
+
+while [ true ] 
+do
+echo "How can I help you today?
+Add
+Subtract
+Exit"
+read operation
+done
