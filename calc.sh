6504ddb7ffa24c628a4bfed6634315a1773c1c2d
Shahrzad Samimi <shahrzadd@gmail.com>
2021-11-10 16:53:44 -0500

    4th commit - Add 'case' statement

--- calc.sh
+++ calc.sh
@@ -13,4 +13,28 @@ Add
 Subtract
 Exit"
 read operation
+
+case $operation in
+    "Add"|"add")
+    echo "Please enter your numbers to add"
+    read NUM_ONE
+    read NUM_TWO
+    echo "This is your sum:"
+    expr $NUM_ONE + $NUM_TWO
+    ;;
+    "Subtract"|"subtract")
+    read NUM_ONE
+    read NUM_TWO
+    echo "This is your difference:"
+    expr $NUM_ONE - $NUM_TWO
+    ;;
+    "Exit"|"exit")
+    echo "Sorry to see you're leaving. Hope to see you soon again $name!"
+    exit
+    ;;
+    *)
+    echo "Sorry, that's not part of my functions. Please choose again or check for typos."
+    ;;
+esac
+
 done
